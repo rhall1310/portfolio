083aa99c3e11dbeb64eb698f466e65d14c510cd6
Rob Hall <r-narch@hotmail.com>
2021-12-24 17:15:18 +0000

    site name change

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'portfolio',
+    title: 'Rob Hall Portfolio',
     htmlAttrs: {
       lang: 'en',
     },
