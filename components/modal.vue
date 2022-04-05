49aed8f82352d1edb124d5dfa9ff72bbb3c1a19d
Rob Hall <r-narch@hotmail.com>
2022-04-05 18:17:43 +0100

    modal start

---
+++ components/modal.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button @click="show = true">Open</button>
+
+    <div
+      class="modal bg-gray-600 w-full h-full fixed top-0 right-0 left-0 z-50"
+      v-show="show"
+    >
+      Boo
+      <button @click="show = false">Close</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+    }
+  },
+}
+</script>
+
+<style></style>
