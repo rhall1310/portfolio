849ae710c2581e6064607369a5c424845da4dcb1
Rob Hall <r-narch@hotmail.com>
2022-04-08 17:47:16 +0100

    modal improvements

--- components/modal.vue
+++ components/modal.vue
@@ -3,11 +3,12 @@
     <button @click="show = true">Open</button>
 
     <div
-      class="modal bg-gray-600 w-full h-full fixed top-0 right-0 left-0 z-50"
+      class="modal bg-gray-600 w-full h-full fixed top-0 right-0 left-0 z-50 flex bg-opacity-40"
       v-show="show"
     >
-      Boo
-      <button @click="show = false">Close</button>
+      <div class="inner-modal bg-black w-1/2 h-1/2 m-auto">
+        <button @click="show = false" class="">Close</button>
+      </div>
     </div>
   </div>
 </template>
