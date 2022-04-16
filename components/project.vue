7b8ca9fd1094c9bcfb21ddbbf90b8e3e32cca02c
Rob Hall <r-narch@hotmail.com>
2022-04-16 16:36:11 +0100

    project improvements

--- components/project.vue
+++ components/project.vue
@@ -1,42 +1,40 @@
 <template>
-  <div class="flex flex-col text-white md:w-1/3 mt-2">
-    <button @click="show = true">Open</button>
+  <div class="text-white md:w-1/3 mt-2">
     <div
-      v-show="show"
-      class="modal bg-gray-600 w-full h-full fixed top-0 right-0 left-0 z-50 flex bg-opacity-40"
+      class="border-neon-dark border-2 p-1 rounded-lg bg-gray-800 m-2 hover:border-neon-light group h-full flex flex-col"
     >
-      <div
-        class="border-neon-dark border-2 p-1 rounded-lg bg-gray-800 m-auto w-1/2 hover:border-neon-light group"
-      >
-        <a target="blank" :href="link">
-          <div class="grid grid-cols-4 grid-rows-5 mx-2 pb-1">
-            <h2
-              class="neon text-neon-light text-lg font-bold row-start-1 h-8 col-start-1 col-span-5 z-10 bg-gray-900 bg-opacity-90 text-center group-hover:underline"
-            >
-              {{ title }}
-            </h2>
-            <img
-              :src="require(`~/assets/images/${this.img}`)"
-              class="row-start-1 row-span-5 col-start-1 col-span-5 w-full"
-            />
-          </div>
-        </a>
-        <p class="neon text-neon-light">
-          {{ text }}
-        </p>
-        <div class="flex flex-row gap-4 font-semibold underline py-2">
-          <a target="blank" :href="code">Source Code</a
-          ><a target="blank" :href="link">Live Demo</a>
-        </div>
-        <div class="neon text-neon-light flex flex-row gap-4 text-4xl">
-          <Icons
-            v-for="icon in icons"
-            :icon="icon"
-            :key="icon.index"
-            class="hover:text-pink-50"
+      <a target="blank" :href="link">
+        <div class="grid grid-cols-4 grid-rows-5 mx-2 pb-1">
+          <h2
+            class="neon text-neon-light text-lg font-bold row-start-1 h-8 col-start-1 col-span-5 z-10 bg-gray-900 bg-opacity-90 text-center group-hover:underline"
+          >
+            {{ title }}
+          </h2>
+          <img
+            :src="require(`~/assets/images/${this.img}`)"
+            class="row-start-1 row-span-5 col-start-1 col-span-5 w-full project-img"
           />
         </div>
-        <button @click="show = false" class="">Close</button>
+      </a>
+      <p class="neon text-neon-light flex-grow">
+        {{ text }}
+      </p>
+
+      <div
+        class="flex flex-row gap-4 font-semibold underline py-2 justify-center"
+      >
+        <a target="blank" :href="code">Source Code</a
+        ><a target="blank" :href="link">Live Demo</a>
+      </div>
+      <div
+        class="neon text-neon-light flex flex-row gap-4 text-4xl justify-center"
+      >
+        <Icons
+          v-for="icon in icons"
+          :icon="icon"
+          :key="icon.index"
+          class="hover:text-pink-50"
+        />
       </div>
     </div>
   </div>
