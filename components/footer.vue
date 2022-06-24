2fd0b2bf923a000932ea77fb93aa213e2164b621
Rob Hall <r-narch@hotmail.com>
2022-06-24 15:41:31 +0100

    p fix

--- components/footer.vue
+++ components/footer.vue
@@ -2,11 +2,11 @@
   <footer
     class="flex bg-gray-900 min-h-2 text-white justify-between py-5 md:mx-8"
   >
-    <p>
+    <div>
       <i class="fas fa-copyright p-2 text-neon">
         <p class="inline text-white">opyright Rob Hall {{ currentYear }}</p></i
       >
-    </p>
+    </div>
     <a href="#info" class="font-semibold self-center mr-4 underline">
       <p class="hidden md:block">Back to top</p>
       <Icons icon="fas fa-arrow-up underline" class="md:hidden" />
@@ -18,7 +18,7 @@
 export default {
   data() {
     return {
-      currentYear: new Date().getFullYear(), // 2020
+      currentYear: new Date().getFullYear(),
     }
   },
 }
